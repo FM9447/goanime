1f44d1688db625047c59672f4b594a6b5a4b3183
FM <fm9447@gmail.com>
2023-10-14 16:52:56 +0000

    Completed All Tasks And Works

---
+++ js/login.js
@@ -0,0 +1,20 @@
+function validateForm() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if (
+    (username === "project" && password === ")(*&^%$#@!") ||
+    (username === "atul" && password === ")(*&^%$#@!") ||
+    (username === "daya" && password === ")(*&^%$#@!") ||
+    (username === "faheem" && password ==="944794")
+    )
+    {
+        alert("SIGNED IN");
+        window.open("index_unlocked.html"); 
+    }
+    else {
+        alert("Invalid Credentials");
+        
+    }
+
+}   
